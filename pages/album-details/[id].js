d1692c3b9336100c375d88c95e37539ab33a4410
DanvanMandela <68956451+DanvanMandela@users.noreply.github.com>
2021-10-17 18:09:06 +0300

    update

--- pages/album-details/[id].js
+++ pages/album-details/[id].js
@@ -1,110 +1,137 @@
-import React, { Component } from "react";
+import React, {Component, useEffect} from "react";
 import Header from "../../components/Header";
 import * as actions from "../../axios/index";
 import MusicHome from "../music-home";
+import {withRouter} from 'next/router';
+
 
 class AlbumDetails extends Component {
-  constructor(props) {
-    super(props);
-    console.log("props here" + "here");
-  }
-  state = {
-    album: "",
-    tracks: [],
-  };
 
-  componentDidMount() {
-    actions.getAlbum(this.props.id).then((album) => {
-      this.setState({
-        album,
-        tracks: album.tracks.data,
-      });
-    });
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            id:props.id,
+            album: "",
+            tracks: [],
+        };
+    }
 
-  renderAlbum = () => {
-    const { album } = this.state;
-    return (
-      <div className="flex w-screen stick sticky top-16 z-30 bg-white">
-        <div className="mx-auto items-center">
-          <div className="flex flex-wrap -mx-1 lg:-mx-4">
-            {/*  Cover */}
-            <div className=" m-1 px-1 md:w-3/4 lg:my-4 lg:px-4 lg:w-1/3 ">
-              {/* Cover Section*/}
-              <article className=" bg-cover overflow-hidden rounded-lg shadow-lg w-[500px] h-[450px] ">
-                <img
-                  alt="Album Cover"
-                  className="block h-full w-full"
-                  src={album.cover_big}
-                />
-              </article>
-              {/* <!-- END Cover Section --> */}
+
+
+
+    componentDidMount() {
+
+        actions.getAlbum(this.state.id).then((album) => {
+            this.setState({
+                album,
+                tracks: album.tracks.data,
+            });
+        });
+    }
+
+    renderAlbum = () => {
+        const {album} = this.state;
+        return (
+            <div className="flex w-screen stick sticky top-16 z-30 bg-white">
+                <div className="mx-auto items-center">
+                    <div className="flex flex-wrap -mx-1 lg:-mx-4">
+                        {/*  Cover */}
+                        <div className=" m-1 px-1 md:w-3/4 lg:my-4 lg:px-4 lg:w-1/3 ">
+                            {/* Cover Section*/}
+                            <article className=" bg-cover overflow-hidden rounded-lg shadow-lg w-[500px] h-[450px] ">
+                                <img
+                                    alt="Album Cover"
+                                    className="block h-full w-full"
+                                    src={album.cover_big}
+                                />
+                            </article>
+                            {/* <!-- END Cover Section --> */}
+                        </div>
+                        {/* END Cover*/}
+
+                        {/* Details  */}
+                        <div className="my-5 px-1 w-full md:w-1/2 lg:my-12 lg:px-4 lg:w-1/3">
+                            {/* <!-- Details Section --> */}
+                            <article className="overflow-hidden rounded-lg shadow-lg w-[400px] h-full">
+                                <header
+                                    className="flex items-center justify-between leading-tight p-2 md:p-4 bg-gray-200">
+                                    <h1 className="text-lg">
+                                        <h1 className="no-underline hover:underline text-black">
+                                            {album.title}
+                                        </h1>
+                                    </h1>
+                                    <div>
+                                        <p className="text-grey-darker text-sm">Release Date</p>
+                                        <p className="text-grey-darker text-sm">
+                                            {album.release_date}
+                                        </p>
+                                    </div>
+                                </header>
+                                <a href="#">
+                                    <img
+                                        alt="Placeholder"
+                                        className="block h-auto w-full"
+                                        src={album.cover_big}
+                                    />
+                                </a>
+
+                                <footer
+                                    className="flex items-center justify-between leading-none p-2 md:p-4 bg-gray-900">
+                                    <a
+                                        className="flex items-center no-underline hover:underline text-white"
+                                        href="#"
+                                    >
+                                        <div>{/* Tracks Here */}</div>
+                                        {/* <p className="ml-2 text-sm">{album.artist.name}</p> */}
+                                    </a>
+                                    <a
+                                        className="no-underline text-grey-darker hover:text-red-dark"
+                                        href="#"
+                                    >
+                                        <span className="text-white">Like</span>
+                                        <i className="fa fa-heart"></i>
+                                    </a>
+                                </footer>
+                            </article>
+                            {/* <!-- END Details Section --> */}
+                        </div>
+                        {/* END Details */}
+                    </div>
+                </div>
             </div>
-            {/* END Cover*/}
+        );
+    };
 
-            {/* Details  */}
-            <div className="my-5 px-1 w-full md:w-1/2 lg:my-12 lg:px-4 lg:w-1/3">
-              {/* <!-- Details Section --> */}
-              <article className="overflow-hidden rounded-lg shadow-lg w-[400px] h-full">
-                <header className="flex items-center justify-between leading-tight p-2 md:p-4 bg-gray-200">
-                  <h1 className="text-lg">
-                    <h1 className="no-underline hover:underline text-black">
-                      {album.title}
-                    </h1>
-                  </h1>
-                  <div>
-                    <p className="text-grey-darker text-sm">Release Date</p>
-                    <p className="text-grey-darker text-sm">
-                      {album.release_date}
-                    </p>
-                  </div>
-                </header>
-                <a href="#">
-                  <img
-                    alt="Placeholder"
-                    className="block h-auto w-full"
-                    src={album.cover_big}
-                  />
-                </a>
+    render() {
+        return (
+            <div>
+                <Header/>
+                <div className="flex justify-between max-w-6xl mx-5  xl:mx-auto pt-2 flex-wrap">
+                    {this.renderAlbum()}
+                </div>
 
-                <footer className="flex items-center justify-between leading-none p-2 md:p-4 bg-gray-900">
-                  <a
-                    className="flex items-center no-underline hover:underline text-white"
-                    href="#"
-                  >
-                    <div>{/* Tracks Here */}</div>
-                    {/* <p className="ml-2 text-sm">{album.artist.name}</p> */}
-                  </a>
-                  <a
-                    className="no-underline text-grey-darker hover:text-red-dark"
-                    href="#"
-                  >
-                    <span className="text-white">Like</span>
-                    <i className="fa fa-heart"></i>
-                  </a>
-                </footer>
-              </article>
-              {/* <!-- END Details Section --> */}
+                <MusicHome/>
             </div>
-            {/* END Details */}
-          </div>
-        </div>
-      </div>
-    );
-  };
+        );
+    }
+}
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <div className="flex justify-between max-w-6xl mx-5  xl:mx-auto pt-2 flex-wrap">
-          {this.renderAlbum()}
-        </div>
+//Add this tow functions
+export async function getStaticProps(context) {
 
-        <MusicHome />
-      </div>
-    );
-  }
+    return {
+        props: {
+            id: context.params.id
+        }, // will be passed to the page component as props
+    }
+}
+
+export const getStaticPaths = async () => {
+
+    return {
+        paths: [], //indicates that no page needs be created at build time
+        fallback: 'blocking' //indicates the type of fallback
+    }
 }
 
 export default AlbumDetails;
--- pages/music-home.js
+++ pages/music-home.js
@@ -1,97 +1,106 @@
-import { Component } from "react";
+import {Component} from "react";
 import * as actions from "../axios";
 import {
-  ClockIcon,
-  MusicNoteIcon,
-  ViewBoardsIcon,
+    ClockIcon,
+    MusicNoteIcon,
+    ViewBoardsIcon,
 } from "@heroicons/react/outline";
 import Link from "next/link";
 import SearchBar from "../components/SearchBar";
 
 class MusicHome extends Component {
-  state = {
-    albums: [],
-  };
-  componentDidMount() {
-    actions.getAlbums().then((item) =>
-      this.setState({
-        albums: item,
-      })
-    );
-  }
+    state = {
+        albums: [],
+    };
 
-  searchAlbums = (term) => {
-    actions.getAlbums(term).then((item) =>
-      this.setState({
-        albums: item,
-      })
-    );
-  };
+    componentDidMount() {
+        actions.getAlbums().then((item) =>
+            this.setState({
+                albums: item,
+            })
+        );
+    }
 
-  renderAlbums = () => {
-    const { albums } = this.state;
-    return albums && albums.length
-      ? albums.map((item, index) => (
-          <div
-            key={index}
-            className="max-w-xs overflow-hidden my-5 justify-center bg-white rounded-lg shadow-lg dark:bg-gray-800"
-          >
-            <img
-              className="object-cover object-center w-[450px] h-56"
-              src={item.album.cover_big}
-              alt="album cover image"
-            />
+    searchAlbums = (term) => {
+        actions.getAlbums(term).then((item) =>
+            this.setState({
+                albums: item,
+            })
+        );
+    };
 
-            <div className="px-6 py-4">
-              <h1 className="text-xl font-semibold text-gray-800  dark:text-white truncate">
-                {item.title}
-              </h1>
+    renderAlbums = () => {
+        const {albums} = this.state;
+        return albums && albums.length
+            ? albums.map((item, index) => (
+                <div
+                    key={index}
+                    className="max-w-xs overflow-hidden my-5 justify-center bg-white rounded-lg shadow-lg dark:bg-gray-800"
+                >
+                    <img
+                        className="object-cover object-center w-[450px] h-56"
+                        src={item.album.cover_big}
+                        alt="album cover image"
+                    />
 
-              <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
-                <ClockIcon className="h-5 w-5 text-gray-500" />
+                    <div className="px-6 py-4">
+                        <h1 className="text-xl font-semibold text-gray-800  dark:text-white truncate">
+                            {item.title}
+                        </h1>
 
-                <h1 className="px-2 text-sm">{item.duration} Seconds</h1>
-              </div>
+                        <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                            <ClockIcon className="h-5 w-5 text-gray-500"/>
 
-              <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
-                <ViewBoardsIcon className="h-5 w-5 text-gray-500" />
+                            <h1 className="px-2 text-sm">{item.duration} Seconds</h1>
+                        </div>
 
-                <h1 className="px-2 text-sm">{item.rank} Fans</h1>
-              </div>
+                        <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
+                            <ViewBoardsIcon className="h-5 w-5 text-gray-500"/>
+
+                            <h1 className="px-2 text-sm">{item.rank} Fans</h1>
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center px-6 py-3 bg-gray-900 align-middle">
+                        <div className="flex ">
+                            <MusicNoteIcon className="h-5 w-5 text-gray-500"/>
+
+                            <h1 className="mx-3 text-lg font-semibold text-white">
+                                {item.artist.name}
+                            </h1>
+                        </div>
+
+                        <Link
+                            href={{
+                                pathname: `/album-details/[id]`,
+                                query: {
+                                    id: item.album.id
+                                },
+                            }}
+                            as={`/album-details/${item.album.id}`}
+                        >
+                            <a className="mx-3 text-sm font-normal bg-gray-200 text-black cursor-pointer p-2 rounded-md hover:scale-125 hover:bg-purple-600 hover:text-white transform transition-all ease-out">
+                                View Tracks
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            ))
+            : null;
+    };
+
+    render() {
+        console.log(this.state);
+        return (
+            <div>
+                <div className="max-w-6xl xl:mx-auto">
+                    <SearchBar searchAlbums={this.searchAlbums}/>
+                    <div className="flex justify-between max-w-6xl mx-5  xl:mx-auto pt-2 flex-wrap">
+                        {this.renderAlbums()}
+                    </div>
+                </div>
             </div>
-            <div className="flex justify-between items-center px-6 py-3 bg-gray-900 align-middle">
-              <div className="flex ">
-                <MusicNoteIcon className="h-5 w-5 text-gray-500" />
-
-                <h1 className="mx-3 text-lg font-semibold text-white">
-                  {item.artist.name}
-                </h1>
-              </div>
-
-              <Link href={`/album-details/${item.album.id}`}>
-                <a className="mx-3 text-sm font-normal bg-gray-200 text-black cursor-pointer p-2 rounded-md hover:scale-125 hover:bg-purple-600 hover:text-white transform transition-all ease-out">
-                  View Tracks
-                </a>
-              </Link>
-            </div>
-          </div>
-        ))
-      : null;
-  };
-
-  render() {
-    console.log(this.state);
-    return (
-      <div>
-        <div className="max-w-6xl xl:mx-auto">
-          <SearchBar searchAlbums={this.searchAlbums} />
-          <div className="flex justify-between max-w-6xl mx-5  xl:mx-auto pt-2 flex-wrap">
-            {this.renderAlbums()}
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default MusicHome;
